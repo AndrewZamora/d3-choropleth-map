70202ec7a16ff1cfabbfed4139b5ef7b9a6aa5ef
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-11-12 00:20:35 +0900

    added fetch for county data

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 (async () => {
-    const eduData = await (await fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")).json()
-    console.log(eduData)
+    const dataLinks = ["https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json", "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"]
+    const requests = dataLinks.map(async link => {
+        return await (await fetch(link)).json()
+    })
+    const [countyData, userEduData] = await Promise.all(requests).catch(error => console.log(error))
+    console.log(countyData, userEduData)
 })()
