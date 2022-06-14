1570cd466fdf0a9596e09e419c2aa2871c8ba2ff
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-06-14 23:23:12 +0900

    got U.S. Map to show

--- index.js
+++ index.js
@@ -13,15 +13,16 @@
   console.log({ countyData, userEduData });
   // Create chart container dimensions
   const margin = { top: 40, right: 80, bottom: 40, left: 80 };
-  const chartHeight = 400;
+  const chartHeight = 1000;
   const chartWidth = 1000;
   const innerHeight = chartHeight - margin.top - margin.bottom;
   const innerWidth = chartWidth - margin.left - margin.right;
   // Create map and projection https://github.com/d3/d3-geo/blob/v3.0.1/README.md#geoPath
   const projection = d3
     .geoAlbersUsa()
-    .translate([innerWidth / 2, innerHeight / 2])
-    .scale([1000]);
+    .scale(1000)
+    // .translate([innerWidth / 2, innerHeight / 2])
+    // .scale(1000);
   const path = d3.geoPath().projection(projection);
   // Select DOM elements and apply dimensions
   const chart = d3
@@ -29,26 +30,14 @@
     .append("svg")
     .attr("height", chartHeight)
     .attr("width", chartWidth);
-  //
-  // chart
-  // .append("g")
-  // .selectAll("path")
-  // .data(countyData.arcs)
-  // .enter()
-  // .append("path")
-  // .attr("d", path)
-  // .style("stroke", "#fff")
-  // .style("stroke-width", "1")
-  // .attr("fill", (d)=> {
-  //   console.log(d)
-  //   return 'green'
-  // })
-  // chart
-  //   .selectAll("path")
-  //   .data(countyData)
-  //   .join("path")
-  //   .attr("d", path)
-  //   .attr("fill", "red")
-  //   .attr("stroke", "green")
-  //   .attr("stroke-width", 1);
+  console.log(topojson.feature(countyData, countyData.objects["counties"]));
+  chart
+    .selectAll("path")
+    .data(topojson.feature(countyData, countyData.objects["nation"]).features)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("fill", (d) => {
+      return "green";
+    });
 })();
