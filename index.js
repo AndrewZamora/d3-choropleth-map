9aab3c137951515db47ce846076dc9365e3d36b4
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-06-16 20:38:27 +0900

    added a color scale

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
     console.log({ error })
   );
   console.log({ countyData, userEduData });
+  // const graphCountyData = countyData.objects["counties"].map()
   // Create chart container dimensions
   const margin = { top: 40, right: 80, bottom: 40, left: 80 };
   const chartHeight = 1000;
@@ -18,10 +19,15 @@
   const innerHeight = chartHeight - margin.top - margin.bottom;
   const innerWidth = chartWidth - margin.left - margin.right;
   // Create map and projection https://github.com/d3/d3-geo/blob/v3.0.1/README.md#geoPath
-  const projection = d3.geoAlbersUsa().scale(1000);
-  // .translate([innerWidth / 2, innerHeight / 2])
-  // .scale(1000);
-  const path = d3.geoPath().projection(projection);
+  const projection = d3
+    .geoAlbersUsa()
+    .translate([innerWidth / 2, innerHeight / 2])
+    .scale(1000);
+  const colorScale = d3
+    .scaleThreshold()
+    .domain([10, 20, 30, 40, 100])
+    .range(d3.schemeReds[5]);
+
   // Select DOM elements and apply dimensions
   const chart = d3
     .select("#chart-container")
@@ -36,6 +42,17 @@
     countyData,
     countyData.objects["counties"]
   ).features;
+  const nation = topojson.feature(
+    countyData,
+    countyData.objects["nation"]
+  ).features;
+  // chart
+  //   .selectAll("path")
+  //   .data(nation)
+  //   .enter()
+  //   .append("path")
+  //   .attr("d", d3.geoPath())
+  //   .attr("class", "nation");
   chart
     .selectAll("path")
     .data(states)
@@ -49,5 +66,18 @@
     .enter()
     .append("path")
     .attr("d", d3.geoPath())
-    .attr("class", "county");
+    .attr("class", "county")
+    .attr("fill", (d) => {
+      const [match] = userEduData.filter((item) => d.id === item.fips);
+      // console.log(match)
+      return colorScale(match.bachelorsOrHigher);
+    })
+    .attr("data-fips", (d) => {
+      const [match] = userEduData.filter((item) => d.id === item.fips);
+      return match.fips;
+    })
+    .attr("data-education", (d) => {
+      const [match] = userEduData.filter((item) => d.id === item.fips);
+      return match.bachelorsOrHigher;
+    });
 })();
