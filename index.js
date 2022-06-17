07bc823429b43f9f05209d2bd050a1cba917348d
AndrewZamora <andrewmichaelzamora@gmail.com>
2022-06-17 22:13:54 +0900

    reorganized map layers and added more comments

--- index.js
+++ index.js
@@ -7,33 +7,31 @@
   const requests = dataLinks.map(async (link) => {
     return await (await fetch(link)).json();
   });
-  const [countyData, userEduData] = await Promise.all(requests).catch((error) =>
+  const [countyData, educationData] = await Promise.all(requests).catch((error) =>
     console.log({ error })
   );
-  console.log({ countyData, userEduData });
-  // const graphCountyData = countyData.objects["counties"].map()
+  let educationDataByFips = {};
+  educationData.forEach((county) => {
+    educationDataByFips[county.fips] = county;
+  });
   // Create chart container dimensions
   const margin = { top: 40, right: 80, bottom: 40, left: 80 };
   const chartHeight = 1000;
   const chartWidth = 1000;
   const innerHeight = chartHeight - margin.top - margin.bottom;
   const innerWidth = chartWidth - margin.left - margin.right;
-  // Create map and projection https://github.com/d3/d3-geo/blob/v3.0.1/README.md#geoPath
-  const projection = d3
-    .geoAlbersUsa()
-    .translate([innerWidth / 2, innerHeight / 2])
-    .scale(1000);
+  // Create colors pattern
   const colorScale = d3
     .scaleThreshold()
     .domain([10, 20, 30, 40, 100])
     .range(d3.schemeReds[5]);
-
   // Select DOM elements and apply dimensions
   const chart = d3
     .select("#chart-container")
     .append("svg")
     .attr("height", chartHeight)
     .attr("width", chartWidth);
+  // Convert topoJson to GeoJson for D3 to handle
   const states = topojson.feature(
     countyData,
     countyData.objects["states"]
@@ -46,13 +44,23 @@
     countyData,
     countyData.objects["nation"]
   ).features;
-  // chart
-  //   .selectAll("path")
-  //   .data(nation)
-  //   .enter()
-  //   .append("path")
-  //   .attr("d", d3.geoPath())
-  //   .attr("class", "nation");
+  // Add elements to map
+  chart
+    .selectAll("path")
+    .data(counties)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", (d) => {
+      return colorScale(educationDataByFips[d.id].bachelorsOrHigher);
+    })
+    .attr("data-fips", (d) => {
+      return educationDataByFips[d.id].fips;
+    })
+    .attr("data-education", (d) => {
+      return educationDataByFips[d.id].bachelorsOrHigher;
+    });
   chart
     .selectAll("path")
     .data(states)
@@ -62,22 +70,9 @@
     .attr("class", "state");
   chart
     .selectAll("path")
-    .data(counties)
+    .data(nation)
     .enter()
     .append("path")
     .attr("d", d3.geoPath())
-    .attr("class", "county")
-    .attr("fill", (d) => {
-      const [match] = userEduData.filter((item) => d.id === item.fips);
-      // console.log(match)
-      return colorScale(match.bachelorsOrHigher);
-    })
-    .attr("data-fips", (d) => {
-      const [match] = userEduData.filter((item) => d.id === item.fips);
-      return match.fips;
-    })
-    .attr("data-education", (d) => {
-      const [match] = userEduData.filter((item) => d.id === item.fips);
-      return match.bachelorsOrHigher;
-    });
+    .attr("class", "nation");
 })();
